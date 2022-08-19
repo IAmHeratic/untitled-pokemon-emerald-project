f95533e81ec00696a621eb2033b658c26a2b8ec1
Jose Rodriguez <jrodr41@gmail.com>
2022-08-18 22:34:41 -0500

    Added House1 to Littleroot Town

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1056,3 +1056,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/LittlerootTown_House1/scripts.inc"
