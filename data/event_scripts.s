968aecf306a147482e553573df66db24e6970d81
iamheratic <jrodr41@gmail.com>
2022-12-25 23:28:47 -0600

    Added House3 to Oldale town

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1060,3 +1060,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/maps/LittlerootTown_House1/scripts.inc"
 
 	.include "data/maps/LittlerootTown_PokemonCenter_1F/scripts.inc"
+
+	.include "data/maps/OldaleTown_House3/scripts.inc"
